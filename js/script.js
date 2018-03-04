74c94afbef4e0be98a27360f0c62116f8b9aa6ea
sabpsuj <sabinapsuj@gmail.com>
2018-03-05 00:02:19 +0100

    Finalversion ofsimple drumkit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('works!');
+function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"`);
+    const key = document.querySelector(`kbd[data-key="${event.keyCode}"`);
+    if(!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+};
+
+function removeTransition(event) {
+   if(event.propertyName !== 'transform') return;
+   this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('kbd');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', playSound);
